1fd26569053c749220869029d7765595944a11f0
giannistefano <gianni.stefano@gmail.com>
2016-03-23 13:33:46 +0100

    add user Gianni in login screen

--- app/src/main/java/com/example/scon/remostapp/login.java
+++ app/src/main/java/com/example/scon/remostapp/login.java
@@ -50,6 +50,7 @@ public class login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
+            ,"gianni.stefano@gmail.com:binnen"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
